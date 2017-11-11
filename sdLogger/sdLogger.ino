d2079d08ef27c13700fd2160957488245bcbae17
nitsuj11595 <justintime4email@gmail.com>
2017-11-11 17:39:05 -0500

    added logging for magnetic switch

--- sdLogger/sdLogger.ino
+++ sdLogger/sdLogger.ino
@@ -9,35 +9,49 @@ SdFile root;
 
 int logCount = 0;
 
+int PIN = A0;
+boolean pinState;
+
 void setup() {
+  pinMode(PIN, INPUT_PULLUP);
   Serial.begin(9600);
   while (!Serial) { }
 
   initSD();
 
   //Count up until new file
-  while(SD.exists(String(logCount) + ".log")){
+  while (SD.exists(String(logCount) + ".log")) {
     logCount++;
   }
 
-  //Create, and write to file
+  //Create and write to file
   theFile = SD.open(String(logCount) + ".log", FILE_WRITE);
   if (theFile) {
-    Serial.print("Creating Log" + logCount);
+    Serial.println("Creating Log#" + String(logCount));
     theFile.println("Log#" + String(logCount));
     theFile.close();
     Serial.println("Done!");
   } else {
     Serial.println("Log#" + String(logCount));
-  }  
-  
+  }
+
 }
 
 void loop() {
-  
+  //Log time of each falling edge
+  if (pinState && !digitalRead(PIN)) {
+    Serial.println("Pressed!");
+    theFile = SD.open(String(logCount) + ".log", FILE_WRITE);
+    if (theFile) {
+      theFile.println(millis());
+      theFile.close();
+    }
+  }
+  pinState = digitalRead(PIN);
+  delay(50);
 }
 
-void initSD(){
+void initSD() {
   //Function to initalize SD card
   Serial.print("Initializing SD card...");
   if (!SD.begin(4)) {
