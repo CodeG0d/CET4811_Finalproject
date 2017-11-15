f14813f2f2a00a36280a1840471ed6af76d5f00c
nitsuj11595 <justintime4email@gmail.com>
2017-11-14 20:04:26 -0500

    code cleanup

--- sdLogger/sdLogger.ino
+++ sdLogger/sdLogger.ino
@@ -4,17 +4,18 @@
 #include <SD.h>
 
 File theFile;
-SdVolume volume;
-SdFile root;
 
 int logCount = 0;
 
 int PIN = A0;
 boolean curState;
 boolean preState;
-unsigned int stateCount = 0;
+unsigned int stateTime = 0;
 unsigned long int preReading = 0;
 unsigned long int preTrig = 0;
+
+char fileExt[] = ".log";
+char filePre[] = "LOG#";
 
 void setup() {
   pinMode(PIN, INPUT_PULLUP);
@@ -22,22 +23,6 @@ void setup() {
   while (!Serial) { }
 
   initSD();
-
-  //Count up until new file
-  while (SD.exists(String(logCount) + ".log")) {
-    logCount++;
-  }
-  
-  //Create and write to file
-  theFile = SD.open(String(logCount) + ".log", FILE_WRITE);
-  if (theFile) {
-    Serial.println("Creating Log#" + String(logCount));
-    theFile.println("Log#" + String(logCount));
-    theFile.close();
-    Serial.println("Done!");
-  } else {
-    Serial.println("Log#" + String(logCount));
-  }
 }
 
 void loop() {
@@ -47,12 +32,27 @@ void loop() {
 
 void initSD() {
   //Function to initalize SD card
-  Serial.print("Initializing SD card...");
   if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
+    Serial.println("NO SD");
     return;
   }
-  Serial.println("initialization done.");
+  
+  //Count up until new file
+  while (SD.exists(String(logCount) + fileExt)) {
+    logCount++;
+  }
+  
+  //Create and write to file
+  theFile = SD.open(String(logCount) + fileExt, FILE_WRITE);
+  if (theFile) {
+    theFile.println(filePre + String(logCount));
+    theFile.close();
+    Serial.println("Created" + String(filePre) + String(logCount));
+  } else {
+    Serial.println(filePre + String(logCount));
+  }
+  
+  Serial.println("SD READY");
 }
 
 void logRotation(){
@@ -62,22 +62,23 @@ void logRotation(){
   curState = digitalRead(PIN);
   if(millis() > preReading){
     if(curState != preState){
-      stateCount += (millis()-preReading);
+      stateTime += (millis()-preReading);
     }
-    else if(stateCount > 0){
-      stateCount -= (millis()-preReading);
+    else if(stateTime > 0){
+      stateTime -= (millis()-preReading);
     }
   }
   preReading = millis();
+  
   //Assume switcing in 100ms to be contact bounce
   //(For 20 in wheel, 100ms = 35.7mph)
-  if(stateCount > 100){
-    stateCount = 0;
+  if(stateTime > 100){
+    stateTime = 0;
     preState = curState;
     //Record time on falling edge
     if(!curState){
       Serial.println(millis() - preTrig);
-      theFile = SD.open(String(logCount) + ".log", FILE_WRITE);
+      theFile = SD.open(String(logCount) + fileExt, FILE_WRITE);
       if (theFile) {
         theFile.println(millis());
         theFile.close();
